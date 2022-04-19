94064eb30f10e10b9366df9fed3b6262b253aefd
Ümit Yaşar Kalkan <uykalkan@gmail.com>
2022-04-19 15:27:38 +0300

    fix tests

--- src/App/index.test.tsx
+++ src/App/index.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {fireEvent, getByText, render, screen} from '@testing-library/react';
 import App from './';
+import AppProvider from "../providers/AppProvider";
 
-test('renders create a job button', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Create a Job/i);
-    expect(linkElement).toBeInTheDocument();
+window.matchMedia = window.matchMedia || function() {return {matches: false,addListener: function() {},removeListener: function() {}};};
+
+test('renders Create a Job button', () => {
+    render(<AppProvider><App /></AppProvider>)
+    const linkElement = screen.getByText(/Create a Job/i)
+    expect(linkElement).toBeInTheDocument()
+})
+
+test('open modal and render Save button', () => {
+    render(<AppProvider><App /></AppProvider>)
+    const linkElement = screen.getByText(/Create a Job/i)
+
+    fireEvent.click(linkElement)
+
+    const modalElement = screen.getByText(/Save/i)
+
+    expect(modalElement).toBeInTheDocument()
 });
