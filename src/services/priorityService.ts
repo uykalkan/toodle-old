f28ed7108a4015c172973d7ac5fb0343f1c9ed5d
Ümit Yaşar Kalkan <uykalkan@gmail.com>
2022-04-19 05:22:22 +0300

    quick fix vercel

--- src/services/priorityService.ts
+++ src/services/priorityService.ts
@@ -1,7 +1,7 @@
 import {Priority} from "../types/Priority";
 
 const getAll = () : Promise<Priority[]> => {
-  return fetch(`${ process.env.API_URL || 'http://localhost:3001' }/priorities`).then(resp => resp.json())
+  return fetch(`${ process.env.REACT_APP_VERCEL_ENV ? 'https://toodle-backend.vercel.app': 'http://localhost:3001' }/priorities`).then(resp => resp.json())
 }
 
 export default {getAll}
